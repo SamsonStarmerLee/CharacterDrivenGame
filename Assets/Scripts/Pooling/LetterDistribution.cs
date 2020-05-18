366fe41cdb40d66ae320a7baf0760126ead7a154
Samson <samson.starmerlee@gmail.com>
2020-05-18 19:20:58 +1000

    Setup letter distribution asset

---
+++ Assets/Scripts/Pooling/LetterDistribution.cs
@@ -0,0 +1,22 @@
+﻿using Boo.Lang;
+using UnityEngine;
+
+namespace Assets.Scripts.Pooling
+{
+    [System.Serializable]
+    public class LetterCount
+    {
+        [SerializeField]
+        string letters;
+
+        [SerializeField]
+        int count;
+    }
+
+    [CreateAssetMenu]
+    public class LetterDistribution : ScriptableObject
+    {
+        [SerializeField]
+        LetterCount[] distribution;
+    }
+}
