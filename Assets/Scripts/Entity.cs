7063f08beba52b3a0a6aa2742f054934b615c6fa
Samson <samson.starmerlee@gmail.com>
2020-05-30 16:55:15 +1000

    Update blowup particle material

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -72,7 +72,6 @@ public abstract class Entity : MonoBehaviour, IOccupant, IDestroy, IInit
         var obj = Instantiate(deathParticle, transform.position, Quaternion.identity);
         var particleSystem = obj.GetComponent<ParticleSystem>();
         var main = particleSystem.main;
-        main.startColor = defaultColor;
 
         Destroy(gameObject);
     }
